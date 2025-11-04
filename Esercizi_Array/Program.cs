d411b9ad1f62750d089a659a0457d4311877b4eb
manciolove <114284283+manciolove@users.noreply.github.com>
2025-11-04 10:53:28 +0100

    Aggiungere i file di progetto.

---
+++ Esercizi_Array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Schema;
+
+namespace Esercizi_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //numero al cotrario
+
+            int[] numContrario = {4,7,8,2,9};
+            
+            for (int i = numContrario.Length-1; i >= 0;  i--)
+            {
+                Console.Write("[" + numContrario[i] + "]");
+            }
+           
+        }
+    }
+}
