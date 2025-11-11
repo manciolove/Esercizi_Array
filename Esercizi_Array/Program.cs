74eea8c40364814d1aa13275d4223b31638f1d43
manciolove <114284283+manciolove@users.noreply.github.com>
2025-11-11 10:20:57 +0100

    aggiunta es 7,8,9,10

--- Esercizi_Array/Program.cs
+++ Esercizi_Array/Program.cs
@@ -8,9 +8,9 @@ namespace Esercizi_Array
         {
             //esercizio 1
 
-            int[] numContrario = {4,7,8,2,9};
-            
-            for (int i = numContrario.Length-1; i >= 0;  i--)
+            int[] numContrario = { 4, 7, 8, 2, 9 };
+
+            for (int i = numContrario.Length - 1; i >= 0; i--)
             {
                 Console.Write("[" + numContrario[i] + "]");
             }
@@ -78,5 +78,130 @@ namespace Esercizi_Array
             {
                 Console.Write("[" + vet3[i] + "]");
             }
+
+            //esercizio 7
+            static void Main(string[] args)
+            {
+                int[] vet = { 1, 2, 5, 10, 7, 6, 13 };
+
+                int a, b, cont = 0;
+
+                Console.WriteLine("Inserisci il primo estremo");
+                a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Inserisci il secondo estremo");
+                b = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < vet.Length; i++)
+                {
+
+                    if (vet[i] >= a && vet[i] <= b)
+                    {
+
+                        cont = cont + 1;
+
+                    }
+
+                }
+
+                Console.WriteLine("Numeri compresi: " + cont);
+
+                Console.WriteLine();
+
+                // esercizio 8
+
+                int[] vet1 = { 1, 2, 5, 10, 7, 6, 13 };
+
+                int cont1 = 0;
+
+                Console.WriteLine("Inserisci il primo estremo");
+                a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Inserisci il secondo estremo");
+                b = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < vet1.Length; i++)
+                {
+
+                    if (vet1[i] <= a || vet1[i] >= b)
+                    {
+
+                        cont1 = cont1 + 1;
+
+                    }
+
+                }
+
+                Console.WriteLine("Numeri non compresi: " + cont1);
+
+                Console.WriteLine();
+
+                //esercizio 9
+
+                int[] vet3 = { 3, 8, 5, 9, 12, 1 };
+
+                int c, d, cont2 = 0;
+
+                Console.WriteLine("Dimmi il primo estremo del primo intervallo");
+                a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Dimmi il secondo estremo del primo intervallo");
+                b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Dimmi il primo estremo del secondo intervallo");
+                c = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Dimmi il secondo estremo del secondo intervallo");
+                d = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < vet3.Length; i++)
+                {
+
+                    if (vet3[i] >= a && vet3[i] <= b || vet3[i] >= c && vet3[i] <= d)
+                    {
+                        cont2 = cont2 + 1;
+                    }
+
+                }
+
+                Console.WriteLine("Numeri tra a-b oppure c-d: " + cont2);
+
+                Console.WriteLine();
+
+                //esercizio 10
+
+                int[] vet4 = { 3, 8, 5, 9, 12, 1 };
+
+                int cont3 = 0; 
+
+                Console.WriteLine("Dimmi il primo estremo del primo intervallo");
+                a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Dimmi il secondo estremo del primo intervallo");
+                b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Dimmi il primo estremo del secondo intervallo");
+                c = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Dimmi il secondo estremo del secondo intervallo");
+                d = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < vet4.Length; i++)
+                {
+
+                    if (vet3[i] >= a && vet3[i] <= b || vet3[i] >= c && vet3[i] <= d)
+                    {
+                    }
+                    else
+                    {
+                        cont3 = cont3 + 1;
+                    }
+
+                }
+
+                Console.WriteLine("Numeri non compresi tra a-b oppure c-d: " + cont3);
+                
+            }
         }
+    }
 }
